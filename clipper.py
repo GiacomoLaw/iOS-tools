d1bf25c4cce8baa00d46adfcbecbcbc8baa442bc
Giacomo Lawrance <giacomolawrance@gmail.com>
2017-08-22 14:50:47 +0100

    added base clipper

---
+++ clipper.py
@@ -0,0 +1,31 @@
+# import the clipboard and view it
+
+import clipboard as cb
+
+cbget = cb.get()
+cleared = ''
+
+
+if cbget == '':
+	print('There is no text on the clipboard!')
+
+# ask to delete the clipboard if there is text detected
+else:
+	print(cbget)
+	
+	print("""
+-------
+Do you want to delete this text?
+
+Yes/No""")
+
+	user_input = input(": ")
+
+	if user_input == "yes":
+		cb.set(cleared)
+		print("\nAll clear!")
+	elif user_input == "Yes":
+		cb.set(cleared)
+		print("\nAll clear!")
+	else:
+		print('\nThanks for using!')

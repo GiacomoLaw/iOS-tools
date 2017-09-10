3b33f92fd940e32285cd3b4a3fe1c4ea2b6282fa
Giacomo Lawrance <giacomolawrance@gmail.com>
2017-09-10 21:42:31 +0100

    added next birthday finder

---
+++ next_birthdays.py
@@ -0,0 +1,24 @@
+import contacts
+from datetime import datetime
+import operator
+
+days_list = []
+people = contacts.get_all_people()
+now = datetime.now()
+for p in people:
+	b = p.birthday
+	if b:
+		next_birthday = datetime(now.year, b.month, b.day)
+		if next_birthday < now:
+			next_birthday = datetime(now.year + 1, b.month, b.day)
+		days = (next_birthday - now).days
+		days_list.append({'name': p.full_name, 'days': days})
+
+if not days_list:
+	print('You don\'t have any birthdays in your address book.')
+else:
+	days_list.sort(key=operator.itemgetter('days'))
+	print('Next Birthdays')
+	print('=' * 20)
+	for item in days_list:
+		print('* %s in %i days' % (item['name'], item['days']))

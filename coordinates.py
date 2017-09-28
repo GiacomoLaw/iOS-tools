a1f4daf01a0e63856f2384a8af329fb04e6708b4
Giacomo Lawrance <giacomolawrance@gmail.com>
2017-09-28 15:31:51 +0100

    deleted coordinates.py

--- coordinates.py
+++
@@ -1,6 +0,0 @@
-# get your coordinates
-
-import location
-address_dict = {'Street': 'Infinite Loop', 'City': 'Cupertino', 'Country': 'USA'}
-results = location.geocode(address_dict)
-print(results)
